e8d6a5578ff68a8b7553669a2bb7508bf8ca5108
Mateus Chuqui <mateuschuqui@gmail.com>
2024-07-30 18:32:12 -0300

    fix: :art: add OrdeRepositoryInterface

--- workspace/src/infrastructure/repository/order.repository.ts
+++ workspace/src/infrastructure/repository/order.repository.ts
@@ -1,9 +1,10 @@
 import Order from "../../domain/entity/order";
 import OrderItem from "../../domain/entity/order_item";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface{
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -81,7 +82,6 @@ export default class OrderRepository {
     const orderModels = await OrderModel.findAll({
       include: ["items"],
     });
-    console.log(orderModels)
     const orders: Order[] = orderModels.map((orderModel) => {
       const orderItems: OrderItem[] = orderModel.items.map(item => {
         return new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity);
